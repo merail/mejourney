965d5d3598b664cdd2244c8824187f157e38c3a1
merail <r.mesherov@blanc.ru>
2024-08-05 11:39:04 +0300

    Remove empty elements from content string split

--- core/src/main/java/merail/life/core/extensions/StringExtensions.kt
+++ core/src/main/java/merail/life/core/extensions/StringExtensions.kt
@@ -2,4 +2,4 @@ package merail.life.core.extensions
 
 fun String.splitWithDelimiter(
     delimiter: String,
-) = Regex("((?<=%1\$s)|(?=%1\$s))".format(delimiter)).split(this)
+) = Regex("((?<=%1\$s)|(?=%1\$s))".format(delimiter)).split(this).filter { it.isNotBlank() }
